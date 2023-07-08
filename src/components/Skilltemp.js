1601ceccf5fbdedfc32886c331bbbbee5369fae8
Venkata Lokesh <76467286+vlokesh08@users.noreply.github.com>
2023-07-08 12:10:17 +0530

    added projects

--- src/components/Skilltemp.js
+++ src/components/Skilltemp.js
@@ -5,7 +5,19 @@ const projects = [
         description: "The virtual mouse using OpenCV, Python, and MediaPipe is a computer vision-based system that enables users to control the cursor on their screen without physical mouse input. By leveraging hand tracking and gesture recognition algorithms, it detects hand movements and translates them into mouse actions, providing an intuitive and hands-free interaction experience.",
         proLink: "https://github.com/vlokesh08/Virtual-Mouse-by-AI"
     },
+    {
+        title:"DetUser",
+        imageUrl:"https://user-images.githubusercontent.com/76467286/251939926-de548938-99d9-49c5-8790-d2583eb0d940.png",
+        description:"This Project gives the User information randomly from the api",
+        proLink: "https://github.com/vlokesh08/DetUser"
+    },
+    {
+        title:"Simple Todo",
+        imageUrl:"https://user-images.githubusercontent.com/76467286/250376252-285c19c5-fbda-4672-9281-4a6dc1144c05.png",
+        description: "Keep the daily works to be done and be noted using this",
+        proLink:"https://github.com/vlokesh08/Simple-TodoList"
 
+    }
 
 ];
 
