1eb327080f2ef102f015cdd6e3cb330b32178b94
vlokesh08 <venkatalokeshvl@gmail.com>
2024-04-03 12:19:14 +0530

    added today learnings

--- src/components/pages/learnings/posts.js
+++ src/components/pages/learnings/posts.js
@@ -1,6 +1,6 @@
 const posts = [
     {
-      date: "2024-04-01",
+      date: "01-04-2024",
       summary: "Got the more details of Docker how it works and how to use it.",
       tags: ["Docker"]
     },
@@ -9,7 +9,11 @@ const posts = [
       summary: "Doing the project HelpPro on React and Redux.",
       tags: ["Frontend", "Backend","React","Redux","Express"]
     },
-    
+    {
+      date: "03-04-2024",
+      summary: "Started by solving the problems on LeetCode.",
+      tags: ["LeetCode"]
+    },
     ]
 
 export default posts;
