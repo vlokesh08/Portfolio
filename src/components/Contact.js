f29f37fc3dc409f2c74b839caeb6e343b4411849
Venkata Lokesh <venkatalokeshvl@gmail.com>
2024-07-30 13:22:42 +0530

    changes contact us page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,127 +1,89 @@
 import React from "react";
 import { useState } from "react";
+import { Input } from "./ui/input";
+import { Textarea } from "./ui/textarea";
+import axios from "axios";
+import { Linkedin } from 'lucide-react';
+import { Github } from 'lucide-react';
+import { Twitter } from 'lucide-react';
 
 function Contact() {
-  const [inputname, setInputName] = useState("");
-  const [inputmail, setInputMail] = useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
+  const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
+  const handleSave = async () => {
+    try{
+      const res = await axios.post(`${BACKEND_URL}/messages`, {
+        name,
+        email,
+        message,
+      });
+      console.log(res);
+    }
+    catch(err){
+      console.log(err);
+    }
+  }
 
   return (
-    <section id="contact">
-      <div className="container lg:pt-5">
-        <div className=" text-center">
-          <h1 className="text-popp font-[700] text-3xl">Contact Me</h1>
-          <br></br>
-        </div>
-        <br></br>
-        <div className="md:flex justify-between items-center">
-          <div className="w-full h-[300px] md:w-1/2 sm:h-[450px]">
-            <iframe
-              className="w-full h-full border-0"
-              title="google-maps"
-              allowFullScreen=""
-              src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d1271.0053568815906!2d79.96758555975659!3d14.420742490303219!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sin!4v1687250759506!5m2!1sen!2sin"
-            ></iframe>
+    <section id="contact" className="min-h-screen">
+        <div className="flex flex-col gap-12 justify-center items-center">
+          <div className="flex flex-col justify-center items-center">
+            <h2 className="text-3xl font-bold tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl">Contact Me</h2>
+            <p className="max-w-2xl mt-3 text-lg leading-6 text-gray-500 dark:text-gray-400">Have a question or want to work together?</p>
           </div>
-
-          <div className="w-full mt-8 md:mt-0 md:w-1/2 sm:h-[450px] lg:flex items-center  px-7 ml-5 py-4 lg:px-8 ">
-            <div class="bg-[#011c2b] rounded-lg p-6 max-lg:text-center">
-              <h2 class="text-xl font-bold text-white">Contact Information</h2>
-              <p class="text-sm text-gray-400 mt-3">
-                Want to share your thoughts? Feel free to drop me a line.
-              </p>
-              <ul class="mt-16 space-y-10">
-                <li class="flex items-center max-lg:justify-center">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="16px"
-                    height="16px"
-                    fill="#fff"
-                    viewBox="0 0 479.058 479.058"
-                  >
-                    <path
-                      d="M434.146 59.882H44.912C20.146 59.882 0 80.028 0 104.794v269.47c0 24.766 20.146 44.912 44.912 44.912h389.234c24.766 0 44.912-20.146 44.912-44.912v-269.47c0-24.766-20.146-44.912-44.912-44.912zm0 29.941c2.034 0 3.969.422 5.738 1.159L239.529 264.631 39.173 90.982a14.902 14.902 0 0 1 5.738-1.159zm0 299.411H44.912c-8.26 0-14.971-6.71-14.971-14.971V122.615l199.778 173.141c2.822 2.441 6.316 3.655 9.81 3.655s6.988-1.213 9.81-3.655l199.778-173.141v251.649c-.001 8.26-6.711 14.97-14.971 14.97z"
-                      data-original="#000000"
-                    />
-                  </svg>
-                  <a href="javascript:void(0)" class="text-white text-sm ml-3">
-                    <strong>venkatalokeshvl@gmail.com</strong>
-                  </a>
-                </li>
-                
-                <li class="flex items-center max-lg:justify-center">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="16px"
-                    height="16px"
-                    fill="#fff"
-                    viewBox="0 0 368.16 368.16"
-                  >
-                    <path
-                      d="M184.08 0c-74.992 0-136 61.008-136 136 0 24.688 11.072 51.24 11.536 52.36 3.576 8.488 10.632 21.672 15.72 29.4l93.248 141.288c3.816 5.792 9.464 9.112 15.496 9.112s11.68-3.32 15.496-9.104l93.256-141.296c5.096-7.728 12.144-20.912 15.72-29.4.464-1.112 11.528-27.664 11.528-52.36 0-74.992-61.008-136-136-136zM293.8 182.152c-3.192 7.608-9.76 19.872-14.328 26.8l-93.256 141.296c-1.84 2.792-2.424 2.792-4.264 0L88.696 208.952c-4.568-6.928-11.136-19.2-14.328-26.808-.136-.328-10.288-24.768-10.288-46.144 0-66.168 53.832-120 120-120s120 53.832 120 120c0 21.408-10.176 45.912-10.28 46.152z"
-                      data-original="#000000"
-                    ></path>
-                    <path
-                      d="M184.08 64.008c-39.704 0-72 32.304-72 72s32.296 72 72 72 72-32.304 72-72-32.296-72-72-72zm0 128c-30.872 0-56-25.12-56-56s25.128-56 56-56 56 25.12 56 56-25.128 56-56 56z"
-                      data-original="#000000"
-                    ></path>
-                  </svg>
-                  <a href="javascript:void(0)" class="text-white text-sm ml-3">
-                    <strong>Nellore, AndhraPradesh, India</strong>
-                  </a>
-                </li>
-              </ul>
-              <ul class="flex max-lg:justify-center mt-16 space-x-4">
-                <li class="bg-gray-800 hover:bg-gray-900 h-10 w-10 rounded-full flex items-center justify-center shrink-0">
-                  <a href="javascript:void(0)">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="18px"
-                      height="18px"
-                      fill="#fff"
-                      viewBox="0 0 24 24"
-                    >
-                      <path
-                        d="M6.812 13.937H9.33v9.312c0 .414.335.75.75.75l4.007.001a.75.75 0 0 0 .75-.75v-9.312h2.387a.75.75 0 0 0 .744-.657l.498-4a.75.75 0 0 0-.744-.843h-2.885c.113-2.471-.435-3.202 1.172-3.202 1.088-.13 2.804.421 2.804-.75V.909a.75.75 0 0 0-.648-.743A26.926 26.926 0 0 0 15.071 0c-7.01 0-5.567 7.772-5.74 8.437H6.812a.75.75 0 0 0-.75.75v4c0 .414.336.75.75.75zm.75-3.999h2.518a.75.75 0 0 0 .75-.75V6.037c0-2.883 1.545-4.536 4.24-4.536.878 0 1.686.043 2.242.087v2.149c-.402.205-3.976-.884-3.976 2.697v2.755c0 .414.336.75.75.75h2.786l-.312 2.5h-2.474a.75.75 0 0 0-.75.75V22.5h-2.505v-9.312a.75.75 0 0 0-.75-.75H7.562z"
-                        data-original="#000000"
-                      />
-                    </svg>
-                  </a>
-                </li>
-                <li class="bg-gray-800 hover:bg-gray-900 h-10 w-10 rounded-full flex items-center justify-center shrink-0">
-                  <a href="https://www.linkedin.com/in/vlokesh08/">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="18px"
-                      height="18px"
-                      fill="#fff"
-                      viewBox="0 0 511 512"
-                    >
-                      <path
-                        d="M111.898 160.664H15.5c-8.285 0-15 6.719-15 15V497c0 8.285 6.715 15 15 15h96.398c8.286 0 15-6.715 15-15V175.664c0-8.281-6.714-15-15-15zM96.898 482H30.5V190.664h66.398zM63.703 0C28.852 0 .5 28.352.5 63.195c0 34.852 28.352 63.2 63.203 63.2 34.848 0 63.195-28.352 63.195-63.2C126.898 28.352 98.551 0 63.703 0zm0 96.395c-18.308 0-33.203-14.891-33.203-33.2C30.5 44.891 45.395 30 63.703 30c18.305 0 33.195 14.89 33.195 33.195 0 18.309-14.89 33.2-33.195 33.2zm289.207 62.148c-22.8 0-45.273 5.496-65.398 15.777-.684-7.652-7.11-13.656-14.942-13.656h-96.406c-8.281 0-15 6.719-15 15V497c0 8.285 6.719 15 15 15h96.406c8.285 0 15-6.715 15-15V320.266c0-22.735 18.5-41.23 41.235-41.23 22.734 0 41.226 18.495 41.226 41.23V497c0 8.285 6.719 15 15 15h96.403c8.285 0 15-6.715 15-15V302.066c0-79.14-64.383-143.523-143.524-143.523zM466.434 482h-66.399V320.266c0-39.278-31.953-71.23-71.226-71.23-39.282 0-71.239 31.952-71.239 71.23V482h-66.402V190.664h66.402v11.082c0 5.77 3.309 11.027 8.512 13.524a15.01 15.01 0 0 0 15.875-1.82c20.313-16.294 44.852-24.907 70.953-24.907 62.598 0 113.524 50.926 113.524 113.523zm0 0"
-                        data-original="#000000"
-                      />
-                    </svg>
-                  </a>
-                </li>
-                <li class="bg-gray-800 hover:bg-gray-900 h-10 w-10 rounded-full flex items-center justify-center shrink-0">
-                  <a href="https://www.instagram.com/v_lokesh08">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="18px"
-                      height="18px"
-                      fill="#fff"
-                      viewBox="0 0 24 24"
-                    >
-                      <path d="M12 9.3a2.7 2.7 0 1 0 0 5.4 2.7 2.7 0 0 0 0-5.4Zm0-1.8a4.5 4.5 0 1 1 0 9 4.5 4.5 0 0 1 0-9Zm5.85-.225a1.125 1.125 0 1 1-2.25 0 1.125 1.125 0 0 1 2.25 0ZM12 4.8c-2.227 0-2.59.006-3.626.052-.706.034-1.18.128-1.618.299a2.59 2.59 0 0 0-.972.633 2.601 2.601 0 0 0-.634.972c-.17.44-.265.913-.298 1.618C4.805 9.367 4.8 9.714 4.8 12c0 2.227.006 2.59.052 3.626.034.705.128 1.18.298 1.617.153.392.333.674.632.972.303.303.585.484.972.633.445.172.918.267 1.62.3.993.047 1.34.052 3.626.052 2.227 0 2.59-.006 3.626-.052.704-.034 1.178-.128 1.617-.298.39-.152.674-.333.972-.632.304-.303.485-.585.634-.972.171-.444.266-.918.299-1.62.047-.993.052-1.34.052-3.626 0-2.227-.006-2.59-.052-3.626-.034-.704-.128-1.18-.299-1.618a2.619 2.619 0 0 0-.633-.972 2.595 2.595 0 0 0-.972-.634c-.44-.17-.914-.265-1.618-.298-.993-.047-1.34-.052-3.626-.052ZM12 3c2.445 0 2.75.009 3.71.054.958.045 1.61.195 2.185.419A4.388 4.388 0 0 1 19.49 4.51c.457.45.812.994 1.038 1.595.222.573.373 1.227.418 2.185.042.96.054 1.265.054 3.71 0 2.445-.009 2.75-.054 3.71-.045.958-.196 1.61-.419 2.185a4.395 4.395 0 0 1-1.037 1.595 4.44 4.44 0 0 1-1.595 1.038c-.573.222-1.227.373-2.185.418-.96.042-1.265.054-3.71.054-2.445 0-2.75-.009-3.71-.054-.958-.045-1.61-.196-2.185-.419A4.402 4.402 0 0 1 4.51 19.49a4.414 4.414 0 0 1-1.037-1.595c-.224-.573-.374-1.227-.419-2.185C3.012 14.75 3 14.445 3 12c0-2.445.009-2.75.054-3.71s.195-1.61.419-2.185A4.392 4.392 0 0 1 4.51 4.51c.45-.458.994-.812 1.595-1.037.574-.224 1.226-.374 2.185-.419C9.25 3.012 9.555 3 12 3Z"></path>
-                    </svg>
-                  </a>
-                </li>
-              </ul>
+          <div className="flex md:flex-row flex-col gap-3  dark:text-white p-8 w-3/4 ">
+        {/* Left Column */}
+        <div className=" bg-blue-500 rounded-lg p-6 mr-8 md:w-1/3 w-full text-white" >
+          <h2 className="text-2xl font-bold mb-4">Get in touch</h2>
+          
+          
+          <div className="mb-6">
+            <h3 className="font-semibold mb-1">Mail</h3>
+            <p className="text-sm"></p>
+            <p className="text-sm">venkatalokeshvl@gmail.com</p>
+          </div>
+          
+          <div className="">
+            <h3 className="font-semibold mb-3">Social media</h3>
+            <div className="flex space-x-4 items-center">
+              <a href="#" className="text-white hover:text-gray-200 bg-blue-400 p-2 rounded-full">
+                <Linkedin className="" />
+              </a>
+              <a href="#" className="text-white hover:text-gray-200 bg-blue-400 p-2 rounded-full">
+                <Github />
+              </a>
+              <a href="#" className="text-white hover:text-gray-200 bg-blue-400 p-2 rounded-full">
+                <Twitter />
+              </a>
+              <a href="#" className="text-white hover:text-gray-200">
+                <i className="fab fa-twitter"></i>
+              </a>
             </div>
           </div>
         </div>
+  
+        {/* Right Column - Form */}
+        <div className="md:w-2/3 w-full">
+        <div className="flex flex-col gap-3 mb-5">
+        
+        <div className="flex space-x-4">
+          <Input type="text" placeholder="Name" className="w-full dark:bg-slate-700 rounded px-3 py-2" value={name} onChange={(e)=>{setName(e.target.value)}}  />
+        </div>
+        <Input type="email" placeholder="Email" className="w-full  dark:bg-slate-700 rounded px-3 py-2" value={email} onChange={(e)=>{setEmail(e.target.value)}}  />
+        
+        <Textarea placeholder="Tell us how can I help you with" className="w-full dark:bg-slate-700 rounded px-3 py-2 h-24" value={message} onChange={(e)=>{setMessage(e.target.value)}} ></Textarea>
+        </div>
+
+            <button type="submit" className="w-full bg-blue-500 text-white rounded py-2 font-semibold hover:bg-blue-600"
+              onClick={handleSave}
+            >
+              Send Message
+            </button>
+        </div>
       </div>
+        </div>
     </section>
   );
 }
