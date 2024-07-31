5a95a0a9347dc827222404226a1473908e37f4d9
Venkata Lokesh <venkatalokeshvl@gmail.com>
2024-07-31 19:12:23 +0530

    added analytics

--- src/index.js
+++ src/index.js
@@ -4,13 +4,14 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from './components/theme-provider';
-
+import { Analytics } from "@vercel/analytics/react"
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <App />
+      <Analytics />
       </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>
